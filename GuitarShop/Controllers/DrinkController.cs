790ce0e56b297704e0cdd45eb0447b3dc7a803b5
Paramjyot Sandhu <sandhups@mail.uc.edu>
2021-04-15 17:03:44 -0400

    Updated DrinkController Endpoints
    Updated DrinkController Endpoints

--- GuitarShop/Controllers/DrinkController.cs
+++ GuitarShop/Controllers/DrinkController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeerPlaces.Models;
+using BeerPlaces.Models.DataLayer;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,53 @@ namespace BeerPlaces.Controllers
 {
     public class DrinkController : Controller
     {
-        public IActionResult Index()
+       /* public IActionResult Index()
         {
             return View();
+        }*/
+
+        private Repository<Drink> drinks { get; set; }
+        public DrinkController(DrinkFinderContext ctx) => drinks = new Repository<Drink>(ctx);
+
+        public ViewResult Index()
+        {
+            var options = new QueryOptions<Drink>
+            {
+                OrderBy = c => c.Id
+            };
+            return View(drinks.List(options));
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(Drink drink)
+        {
+            if (ModelState.IsValid)
+            {
+                drinks.Insert(drink);
+                drinks.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(drink);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            return View(drinks.Get(id));
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(Drink drink)
+        {
+            drinks.Delete(drink);
+            drinks.Save();
+            return RedirectToAction("Index");
         }
     }
 }
